d1a90f7c2606a96b807aa9cf6538ddd82aab934a
JakeClark-chan <22521350@gm.uit.edu.vn>
2024-03-10 11:41:31 +0700

    Few more things in home.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: ["./templates/**/*.{html,htm}"],
+  content: [
+    "./templates/**/*.{html,htm}",
+    "./static/**/*.{js}",
+  ],
   theme: {
     extend: {
       colors: {
         'blue': '#2c67fe',
         'pink': '#fab6b6',
         'green': '#b3f32b',
+        'lightpink': '#fae5e5',
       }
     },
   },
