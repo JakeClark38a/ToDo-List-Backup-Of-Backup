2e20b35c119a1b2c600d9cb3b39c4e435c9798ce
JakeClark-chan <22521350@gm.uit.edu.vn>
2024-03-12 07:18:04 +0700

    Few more things in home.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,32 @@ module.exports = {
   content: [
     "./templates/**/*.{html,htm}",
     "./static/**/*.js",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
       colors: {
-        'blue': '#2c67fe',
-        'pink': '#fab6b6',
-        'green': '#b3f32b',
-        'lightpink': '#fae5e5',
-      }
+        primary: {
+          red: "#FAB6B6",
+          green: "#9ADAB0",
+          yellow: "#DCCAA0",
+          blue: "#75C6CC",
+        },
+        accent: {
+          red: "#fc9999",
+          green: "#79cc96",
+          yellow: "#cdb784",
+          blue: "#8be0e7",
+        },
+        background: {
+          main: "#FCF6F6",
+          secondary: "#FCF0FF",
+          thirdary: "#F0FEFF",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ]
 }
