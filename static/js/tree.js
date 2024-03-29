ce844240b851487265ed4c40250481c31a9f1576
KienHoSD <channelback546@gmail.com>
2024-03-29 16:38:26 +0700

    update tree

--- static/js/tree.js
+++ static/js/tree.js
@@ -1,67 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var treesContainer = document.getElementById("Trees");
-  
+  var treeCounter = 0; // Counter for trees surpassing tree11
+  var treeCountDisplay = document.getElementById("treeCount");
   // Add a click event listener to the trees container
-  treesContainer.addEventListener("click", function(event) {
+  treesContainer.addEventListener("click", function (event) {
     var target = event.target;
-    if (target.tagName === "IMG") {
+    if (target.classList.contains("currTree")) {
       var currentSrc = target.src;
       var match = currentSrc.match(/tree(\d+)\.gif/);
-      
       if (match && match[1]) {
         var currentNumber = parseInt(match[1], 10);
         var nextNumber = (currentNumber % 11) + 1; // Ensure cycling from 1 to 11
         
         // Construct the new source URL with the next number
         var newSrc = "../static/images/tree" + nextNumber + ".gif";
-        
-        // Change the source of the clicked tree image
-        target.src = newSrc;
-        
-        // Remove click event listener from the clicked tree image
-        target.removeEventListener("click", clickHandler);
-        
-        // If the current number is 11, add another tree image
+
+        // If the current number is 11, meaning the tree is fully grown
         if (currentNumber === 11) {
+          treeCounter++; // Increment the counter when surpassing tree11
           var newTree = document.createElement("img");
-          newTree.src = "../static/images/tree1.gif";
+          treeCountDisplay.innerHTML = "Number of Trees: " + treeCounter;
+          newTree.src = newSrc;
           newTree.alt = "myTree";
-          newTree.className = "m-auto";
-          
-          // Add click event listener to the new tree image
-          newTree.addEventListener("click", clickHandler);
-          
-          // Append the new tree image to the trees container
-          treesContainer.appendChild(newTree);
+          newTree.className = "m-auto tree new-tree currTree"; // Add the new-tree class and currTree class
+          target.src = newSrc;
+        } else {
+          // If not fully grown, simply change the source of the clicked tree image
+          target.src = newSrc;
         }
       }
     }
   });
-  
-  // Initial click event handler for the first tree image
-  function clickHandler(event) {
-    var target = event.target;
-    var currentSrc = target.src;
-    var match = currentSrc.match(/tree(\d+)\.gif/);
-    
-    if (match && match[1]) {
-      var currentNumber = parseInt(match[1], 10);
-      var nextNumber = (currentNumber % 11) + 1; // Ensure cycling from 1 to 11
-      
-      // Construct the new source URL with the next number
-      var newSrc = "../static/images/tree" + nextNumber + ".gif";
-      
-      // Change the source of the clicked tree image
-      target.src = newSrc;
-      
-      // Remove click event listener from the clicked tree image
-      target.removeEventListener("click", clickHandler);
-    }
-  }
-  
-  // Add click event listener to the initial tree image
-  var initialTree = document.getElementById("currTree");
-  initialTree.addEventListener("click", clickHandler);
 });
-
-
