fe71107bff38c3a9b90958f285571c946027782c
SeaW1nd <vodanhlax@gmail.com>
2024-04-28 16:15:14 +0700

    Rebuild flask backend into seperate files and update todo.py in api folder

--- api/todo.py
+++ api/todo.py
@@ -21,6 +21,23 @@ def datetime_to_string(user_date):
     result = datetime.datetime.strptime(user_date, '%Y-%m-%dT%H:%M')
     return result.strftime('%Y-%m-%d %H:%M:%S')
 
+
+def default_group(group_id,group_title,user_id, color):
+    if not Groupss.query.filter_by(group_id=group_id).first():
+        new_group = Groupss(group_id=group_id, group_title=group_title, user_id=user_id, color=color)
+        tododb.session.add(new_group)
+        tododb.session.commit()
+    else:
+        return
+
+def default_tag(tag_id,group_id,tag_title,tag_color,user_id):
+    if not Tags.query.filter_by(tag_id=tag_id).first():
+        new_tag = Tags(tag_id=tag_id, tag_title=tag_title, tag_color=tag_color, user_id=user_id, group_id=group_id)
+        tododb.session.add(new_tag)
+        tododb.session.commit()
+    else:
+        return
+
 #Endpoints
 
 @todo.route('/todo', methods=['GET'])
@@ -28,6 +45,15 @@ def datetime_to_string(user_date):
 def main_page():
     curr_user = Users.query.get(current_user.get_id())
     if curr_user.isFillForm == False:
+        default_group('gid001',"Do",curr_user.user_id,"#7aa5cf")
+        default_group('gid002',"Delegate",curr_user.user_id,"#63c074")
+        default_group('gid003',"Schedule",curr_user.user_id,"#ac7acf")
+        default_group('gid004',"Delete",curr_user.user_id,"#c5e875")
+        default_tag('tag1','gid001','tag1','#7aa5cf',curr_user.user_id)
+        default_tag('tag2','gid002','tag2','#63c074',curr_user.user_id)
+        default_tag('tag3','gid003','tag3','#ac7acf',curr_user.user_id)
+        default_tag('tag4','gid004','tag4','#c5e875',curr_user.user_id)
+        default_tag('tag5','gid001','tag5','#7aa5cf',curr_user.user_id)
         return redirect(url_for('profiles.profile', type=session['type']))
     else:
         return render_template('mainPage.html')
