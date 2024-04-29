254045f9e1fe6335eae4344a45cbe8938082a6ae
Tran Nguyen Tien Thanh <fortconquer909@gmail.com>
2024-04-29 18:56:32 +0700

    link calendar page and tree page to flask

--- app.py
+++ app.py
@@ -1,17 +1,5 @@
 from api.main import create_app
-
 app = create_app()
 
-@app.route('/calendar')
-def calendar():
-    return render_template('calendarPage.html')
-
-@app.route('/calendar.html')
-def calendarPage():
-    return render_template('calendar.html')
-
-@app.route('/treePage')
-def treePage():
-    return render_template('treePage.html')
 if __name__ == '__main__':
     app.run(debug=True, ssl_context='adhoc')
