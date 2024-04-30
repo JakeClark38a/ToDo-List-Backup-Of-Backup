76fc3c43b8dd6a0b6670534b76fe50b8ee20d151
SeaW1nd <vodanhlax@gmail.com>
2024-04-30 15:06:19 +0700

    Fix bug at calendar page and some adjustment in todopage

--- api/calendar.py
+++ api/calendar.py
@@ -1,12 +1,17 @@
 from flask import Flask, redirect, url_for,render_template,Blueprint
+from flask_login import login_required
 
-calendar = Blueprint('calendar', __name__, static_folder='../static/', template_folder='../templates/')
+calendars = Blueprint('calendar', __name__, static_folder='../static/', template_folder='../templates/')
 
 
-@calendar.route('/calendar',methods=['GET'])
+
+@calendars.route('/calendar',methods=['GET'])
+@login_required
 def calendar():
     return render_template('calendarPage.html')
 
-@calendar.route('/calendar.html', methods=['GET'])
-def calendarPage():
-    return render_template('calendar.html')
+
+@calendars.route('/calendarTest.html', methods=['GET'])
+@login_required
+def calendar_test():
+    return render_template('calendarTest.html')
