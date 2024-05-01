f4e28ed9b5db362f9ab6bb41168f6ebdcc5f49c1
lightunderwolft <22521013@gm.uit.edu.vn>
2024-05-02 02:14:34 +0700

    add teampage(group teamspage.html and teampage.html),add action(needed)

---
+++ static/js/CRUB2modal_handlerforteam.js
@@ -0,0 +1,80 @@
+
+const $targetEl2 = document.getElementById('crud-modal2');
+const options = {
+  // placement: 'bottom-right',
+  backdrop: 'dynamic',
+  backdropClasses:
+    'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+  closable: true,
+  onHide: () => {
+    modalteampage.resetState();
+    console.log('modal is hidden');
+  },
+
+  onToggle: () => {
+    console.log('modal has been toggled');
+  },
+};
+if (window.innerWidth < 768) {
+  options.backdrop = 'static';
+}
+const modalteampage = new Modal($targetEl2, options);
+
+modalteampage.resetState = function () {
+  $('#crud-modal2 h3').text("Create");
+  $('#crud-modal2 h3').show();
+
+  $('#crud-modal2 label[for="teamname"]').text("Team's Name");
+  $('#crud-modal2 #name').attr("placeholder", "Team's Name");
+  $('#crud-modal2 #name').val("");
+  $('#crud-modal2 #name-sec').show();
+
+
+
+  $('#crud-modal2 label[for="teamdescription"]').text("Team Description");
+  $('#crud-modal2 #description').val("");
+  $('#crud-modal2 #description').attr("placeholder", "Team Description");
+  $('#crud-modal2 #desc-sec').show();
+}
+
+modalteampage.CreateTeam = function () {
+  let h3 = "Create Team";
+  let teamname = "";
+  let teamdescription = "";
+  $("#crud-modal2 #code-sec").hide();
+  $('#crud-modal2 #teamname-sec').show();
+  $('#crud-modal2 #teamdesc-sec').show();
+  $('#crud-modal2 #Create-sec').show();
+  $('#crud-modal2 #delete-sec').show();
+  $('#crud-modal2 #Join-sec').hide();
+  $('#crud-modal2 label[for="teamname"]').text("Team's Name");
+  $('#crud-modal2 label[for="teamdescription"]').text("Team Description");
+  $('#crud-modal2 h3').text(h3);
+  $('#crud-modal #teamname').val(teamname);
+  $('#crud-modal #teamdescription').val(teamdescription);
+
+  modalteampage.show();
+}
+
+modalteampage.JoinTeam = function () {
+  let h3 = "Join Team";
+  let teamcode = "";
+  $("#crud-modal2 #teamname-sec").hide();
+  $('#crud-modal2 #teamdesc-sec').hide();
+  $('#crud-modal2 #code-sec').show();
+  $('#crud-modal2 #Create-sec').hide();
+  $('#crud-modal2 #delete-sec').hide();
+  $('#crud-modal2 #Join-sec').show ();
+  $('#crud-modal2 label[for="teamcode"]').text("Team's Code");
+  $('#crud-modal2 h3').text(h3);
+  $('#crud-modal2 #teamcode').val(teamcode);
+  modalteampage.show();
+}
+
+$('#btn-close-modal').click(function () {
+  modalteampage.hide();
+});
+
+
+export { modalteampage };
+
--- static/js/list.js
+++ static/js/list.js
@@ -1,30 +1,34 @@
+import {modalteampage} from "./CRUB2modal_handlerforteam.js";
 
 let team = {
     id000001 :{
         name: "Team 1",
         des: "Team 1 description",
+        code: "code1",
         members: [],        
     },
     id000002 :{
         name: "Team 2",
         des: "Team 2 description",
+        code: "code2",
         members: [],        
     },
     id000003 :{
         name: "Team 3",
         des: "Team 3 description",
+        code: "code3",
         members: [],        
     },
 }
-function teamlist(id, name) {
+function teamlist(id, name, code) {
     return (
       ` <div id="`+ id + `"class="flex sm:w-5/6 md:w-3/4 lg:w-4/5 sm:max-w-md md:max-w-xl lg:max-w-screen-xl  h-fit my-3 md:ml-8 lg:ml-0 border-2 rounded-lg  ">
       <div class="inline-block  self-center border-2 rounded-full mx-4 ">
           <img class="w-10 h-10 rounded-full" src="/static/images/profile.jpg" alt="avtr">
       </div>
       <div class="inline-block grow mr-2 ">
-          <p id="`+ name +`">`+ name + `</p>
-          <p id="`+ id + `">`+ id + `</p>
+          <p id="`+ name +`" class="dark:text-white">`+ name + `</p>
+          <p id="`+ code + `" class="dark:text-white">`+ code + `</p>
       </div>
       <div class="inline-block justify-self-end self-center mr-4">
           <button><svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -35,6 +39,8 @@ function teamlist(id, name) {
   </div> ` 
     );
   }
+$(document).ready(function(){
+   
 
     function teamlist1() {
         let teamlist2 = $("#teamlist");
@@ -42,26 +48,77 @@ function teamlist(id, name) {
         for (let key in team) {
             let teamname = team[key].name;
             let teamid = key;
-            teamlist2.append(teamlist(teamid, teamname));
+            let teamcode = team[key].code;
+            teamlist2.append(teamlist(teamid, teamname, teamcode));
         }
     }
 
 
-    function addteam(){
+    function addteam(teamid){ 
         let teamname = $("#teamname").val();
-        let teamdes = $("#teamdes").val();
-        let teamid = "id" + Math.floor(Math.random() * 1000000);
+        let teamdes = $("#teamdescription").val();
+        let teamcode;
+        if (teamid) {
+            let teamcode = team[teamid].code;
+            // let Tid = teamid; // Remove this line
+        } else {
+            teamid = "id" + Math.floor(Math.random() * 1000000);
+            teamcode = "code" + Math.floor(Math.random() * 1000000);
+        }
+        
         team[teamid] = {
             name: teamname,
             des: teamdes,
+            code: teamcode,
             members: [],
-        };        
+        };
     }
-
-    $("#createteam_button").click(function(){
-       
-        addteam();
-        teamlist1();
-        window.location.href ="teamspage.html";
-    });
     teamlist1();
+    $("#create").click(function(){
+        modalteampage.CreateTeam();
+    });
+
+    $("#join").click(function(){
+        modalteampage.JoinTeam();
+    });
+    $("#create1").click(function(){
+        modalteampage.CreateTeam();
+    });
+
+    $("#join1").click(function(){
+        modalteampage.JoinTeam();
+    });
+   
+    modalteampage.resetState();
+   
+        $("#createandjoin").click(function(){
+        $("#teamcreate_join ").show();
+
+        $("#teampage #pageheader").hide();
+        $("#teampage #teamlist").hide();
+        });
+        // don't using not working now
+     function t_join(){
+        let searchCode = $("#teamcode").val();
+        let teamId = Object.keys(team).find(key => team[key].code === searchCode);
+        addteam(teamId);
+     }
+     $("#Join-sec").click(function(){
+            t_join();
+            modalteampage.hide();
+            $("#teamcreate_join ").hide();
+
+            $("#teampage #pageheader").show();
+            $("#teampage #teamlist").show();
+            teamlist1();
+     });
+     $("#Create-sec").click(function(){
+            addteam(null);
+            modalteampage.hide();
+            $("#teamcreate_join ").hide();
+    
+            $("#teampage #pageheader").show();
+            $("#teampage #teamlist").show();
+            teamlist1();
+    });
+});
