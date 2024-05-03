151f0fe2a695ec9aa9203abd9dd5772ae7a5e640
KienHoSD <channelback546@gmail.com>
2024-05-03 23:18:28 +0700

    Improve shop mechanic

--- static/js/tree.js
+++ static/js/tree.js
@@ -295,15 +295,7 @@ document
 
 // Function to unhide the shop modal when the shop button is clicked
 document.getElementById("shop").addEventListener("click", () => {
-  fetch('shop-content.html')
-    .then(response => response.text())
-    .then(data => {
-      $shopModalEl.innerHTML = data;
-      shopModal.show();
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+  shopModal.show();
 });
 
 // Function to center the shop modal
@@ -324,75 +316,75 @@ centerShopModal();
 
 
 
-$(document).ready(function () {
-  // Constants
-  const MAX_TREE_LEVEL = 11;
-  const XP_PER_CLICK = 5;
-  const FERTILIZER_COST = 1;
-  const WATER_COST = 1;
+// $(document).ready(function () {
+//   // Constants
+//   const MAX_TREE_LEVEL = 11;
+//   const XP_PER_CLICK = 5;
+//   const FERTILIZER_COST = 1;
+//   const WATER_COST = 1;
 
-  // Variables
-  let treeCounter = 0;
-  let xp = 0;
-  let food = 999;
-  let water = 999;
+//   // Variables
+//   let treeCounter = 0;
+//   let xp = 0;
+//   let food = 999;
+//   let water = 999;
 
-  // DOM elements
-  const treesContainer = $("#Trees");
-  const treeCountDisplay = $("#treeCount");
-  const fertilizerDisplay = $('#fertilizerCount');
-  const waterDisplay = $('#waterCount');
-  const xpDisplay = $('#xpCount');
+//   // DOM elements
+//   const treesContainer = $("#Trees");
+//   const treeCountDisplay = $("#treeCount");
+//   const fertilizerDisplay = $('#fertilizerCount');
+//   const waterDisplay = $('#waterCount');
+//   const xpDisplay = $('#xpCount');
 
-  // Initialize UI
-  fertilizerDisplay.text(food);
-  waterDisplay.text(water);
-  xpDisplay.text(`Xp: ${xp}/${xpForNextTree()}`);
+//   // Initialize UI
+//   fertilizerDisplay.text(food);
+//   waterDisplay.text(water);
+//   xpDisplay.text(`Xp: ${xp}/${xpForNextTree()}`);
 
-  // Event listener
-  treesContainer.on("click", ".currTree", function (event) {
-    if (food < FERTILIZER_COST || water < WATER_COST) return;
+//   // Event listener
+//   treesContainer.on("click", ".currTree", function (event) {
+//     if (food < FERTILIZER_COST || water < WATER_COST) return;
 
-    const $target = $(event.target);
+//     const $target = $(event.target);
 
-    if ($target.hasClass("currTree")) {
-      const currentSrc = $target.attr("src");
-      const match = currentSrc.match(/tree(\d+)\.gif/);
+//     if ($target.hasClass("currTree")) {
+//       const currentSrc = $target.attr("src");
+//       const match = currentSrc.match(/tree(\d+)\.gif/);
 
-      if (match && match[1]) {
-        const currentNumber = parseInt(match[1], 10);
-        const nextNumber = (currentNumber % MAX_TREE_LEVEL) + 1;
+//       if (match && match[1]) {
+//         const currentNumber = parseInt(match[1], 10);
+//         const nextNumber = (currentNumber % MAX_TREE_LEVEL) + 1;
 
-        // Update resources
-        food -= FERTILIZER_COST;
-        water -= WATER_COST;
-        xp += 1;
+//         // Update resources
+//         food -= FERTILIZER_COST;
+//         water -= WATER_COST;
+//         xp += 1;
 
-        // Update UI
-        fertilizerDisplay.text(food);
-        waterDisplay.text(water);
-        xpDisplay.text(`Xp: ${xp}/${xpForNextTree()}`);
+//         // Update UI
+//         fertilizerDisplay.text(food);
+//         waterDisplay.text(water);
+//         xpDisplay.text(`Xp: ${xp}/${xpForNextTree()}`);
 
-        // Grow tree
-        if (xp >= xpForNextTree()) {
-          xp -= xpForNextTree();
-          treeCounter++;
-          treeCountDisplay.text(`Number of Trees: ${treeCounter}`);
-          const newTree = $("<img />", {
-            src: `../static/images/tree${nextNumber}.gif`,
-            alt: "myTree",
-            class: "m-auto tree new-tree currTree"
-          });
-          $target.attr("src", `../static/images/tree${nextNumber}.gif`).after(newTree);
-        } else {
-          $target.attr("src", `../static/images/tree${nextNumber}.gif`);
-        }
-      }
-    }
-  });
+//         // Grow tree
+//         if (xp >= xpForNextTree()) {
+//           xp -= xpForNextTree();
+//           treeCounter++;
+//           treeCountDisplay.text(`Number of Trees: ${treeCounter}`);
+//           const newTree = $("<img />", {
+//             src: `../static/images/tree${nextNumber}.gif`,
+//             alt: "myTree",
+//             class: "m-auto tree new-tree currTree"
+//           });
+//           $target.attr("src", `../static/images/tree${nextNumber}.gif`).after(newTree);
+//         } else {
+//           $target.attr("src", `../static/images/tree${nextNumber}.gif`);
+//         }
+//       }
+//     }
+//   });
 
-  // Function to calculate XP required for the next tree
-  function xpForNextTree() {
-    return treeCounter * 10 + 10;
-  }
-});
+//   // Function to calculate XP required for the next tree
+//   function xpForNextTree() {
+//     return treeCounter * 10 + 10;
+//   }
+// });
