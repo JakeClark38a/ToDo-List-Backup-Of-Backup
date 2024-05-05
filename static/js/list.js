040862ed53ac59e42a24e1cb970e768a15874f70
lightunderwolft <22521013@gm.uit.edu.vn>
2024-05-05 11:31:44 +0700

    just commit

--- static/js/list.js
+++ static/js/list.js
@@ -50,15 +50,15 @@ function teamlist(id, name, des) {
           <p id="`+ name + `" class="dark:text-white">` + name + `</p>
           <p id="`+ des + `" class="dark:text-white">` + des + `</p>
       </div>
-      <div class="inline-block justify-self-end self-center mr-4">
-                        <button id="dropdownMenuIconButton `+ id + `" data-dropdown-trigger="click"  data-dropdown-toggle="dropdownDots-`+ id +`" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
+      <div id="div_button `+ id +`" class="inline-block justify-self-end self-center mr-4">
+                        <button id="dropdownMenuIconButton `+ id + `" data-dropdown-trigger="click"  data-dropdown-toggle="dropdownDots `+ id +`" class="dropdown_button inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
                             <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
                             <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
                             </svg>
                             </button>
                             
                             <!-- Dropdown menu -->
-                            <div id="dropdownDots-`+ id +`" class="z-10 hidden border-2 border-white bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                            <div id="dropdownDots `+ id +`" class="dropdown z-10 hidden border-2 border-white bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                 <ul class="py-2 w-full text-sm text-gray-700 dark:text-white" aria-labelledby="dropdownMenuIconButton">
                                   <li>
                                     <button class="editteam block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</button>
@@ -70,9 +70,8 @@ function teamlist(id, name, des) {
                                
                             </div>
                             
-                    </div>
-  </div>
-   `);
+        </div>
+    </div>`);
     }
     else {
         return (
@@ -257,5 +256,13 @@ $(document).ready(function () {
         console.log(teamid);
         
         modalteampage.editTeam(team[teamid]);
+    });
+    // click the dropdown button on team(create)'s team that is created by user
+    $("#teamlist2").on("click", ".dropdown_button", function () {
+        let id = $(this).attr("id");
+        console.log(id);
+        let dropdown = $(this).attr("data-dropdown-toggle");
+        console.log(dropdown);
+        $("#" + dropdown).toggle();
     });
 });
