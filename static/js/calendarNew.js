e1bc43ff121ecc83a8cbce8cdad3478279569ffe
JakeClark-chan <22521350@gm.uit.edu.vn>
2024-05-05 21:34:42 +0700

    Update calendarNew.js to patch task read-only into project page

--- static/js/calendarNew.js
+++ static/js/calendarNew.js
@@ -175,6 +175,7 @@ let monthDict = {
     "November": 11,
     "December": 12,
 };
+// Function to combine day, month, year to format: March 7th, 2024
 const combineMonth = function (day, month, year) {
     // Add st, nd, rd, th to day
     if (day == 1 || day == 21 || day == 31) {
@@ -196,7 +197,7 @@ const combineMonth = function (day, month, year) {
     // Return as: March 7th, 2024
     return month + " " + day + ", " + year;
 }
-
+// Function to get day, month, year from focused datepicker-cell
 const pickDate = function () {
     var day = undefined;
     // Get day from focused datepicker-cell
@@ -219,7 +220,7 @@ const pickDate = function () {
 
     return [day, month, year];
 }
-
+// Function to get all tasks that have deadline on focused day
 const getTask = function (day, month, year) {
     // Get all tasks that have deadline on focused day (in args)
     // Return as array of tasks ID
@@ -234,7 +235,7 @@ const getTask = function (day, month, year) {
     }
     return tasks;
 }
-
+// Function to get all days that have deadline before today
 const getExpiredDays = function (month, year) {
     // Get all days that have deadline in month and year
     // Steps: Get deadline of each task, if it is in month and year, add its day to array
@@ -249,7 +250,7 @@ const getExpiredDays = function (month, year) {
     console.log("Days", days);
     return days;
 }
-
+// Function to change color of days that have deadline before today
 const changeExpiredColor = (days) => {
     // Change color of days that have deadline before today
     // to red
@@ -275,19 +276,19 @@ const changeExpiredColor = (days) => {
         // Change color of days that have deadline
         if (days[day]) {
             if (day >= 22 && countCell < 7) {
-                $(this).css("color", "black");
+                $(this).css("color", "");
             } else if (day <= 13 && countCell > 28) {
-                $(this).css("color", "black");
+                $(this).css("color", "");
             } else {
                 $(this).css("color", "red");
             }
         } else {
-            $(this).css("color", "black");
+            $(this).css("color", "");
         }
         countCell++;
     });
 
-} // Calendar: Saturday evening
+}
 
 // All steps:
 // Step 1: Load Dict using AJAX - todo - done
@@ -298,8 +299,7 @@ const changeExpiredColor = (days) => {
 
 // Use jQuery
 
-// Step 1 
-// 
+// Function to load data from server
 var Dict = {};
 const loadDict = function () {
     return new Promise(function (resolve) {
@@ -311,7 +311,7 @@ const loadDict = function () {
     });
 }
 
-
+// Function to append tasks to Task-Section
 const appendTask = function (Dict, day, month, year) {
     $("#Task-Section").empty();
 
@@ -322,9 +322,14 @@ const appendTask = function (Dict, day, month, year) {
             $("#Task-Section").append(MainScreen.TaskTemplate(taskId, Dict.tasks[taskId]));
         }
     }
+
+    // Also remove all task edit, delete and complete button
+    $(".Task-Edit").hide();
+    $("#Task-Cancel").hide();
+    $("#Task-Destroyer").hide();
 }
 
-
+// Function to map datepicker to table
 const mapDatepickerToTable = function (){
     // Map datepicker #calendar to #CalendarTable
     // Get all div with class dow
@@ -362,22 +367,97 @@ const mapDatepickerToTable = function (){
     var clonedText = $("button.view-switch").clone();
     // Disable clock with change id of #clock to #month 
     // Clone the text, also change class from text-sm to text-xl
-    $('#clock').attr('id', 'month').html(clonedText).children().first().removeClass("text-sm view-switch").addClass("text-xl");
+    $('#clock').attr('id', 'month');
+    $('#month').html(clonedText).children().first().removeClass("text-sm view-switch").addClass("text-xl");
+}
+
+/*
+Sample bottom tooltip:
+<button data-tooltip-target="tooltip-bottom" data-tooltip-trigger="click" data-tooltip-placement="bottom" type="button" class="ms-3 mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Task title</button>
+
+<div id="tooltip-bottom" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+    <h3>Task title</h3>
+    <p>
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+        </svg>
+        Deadline: 2024-05-07T11:17
+    </p>
+    <p>
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"/>
+        </svg>
+        Description: About making a website
+    </p>
+    <p>
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+        </svg>
+        Tags: tag1
+    </p>
+    <p>
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.5 8H4m0-2v13a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1h-5.032a1 1 0 0 1-.768-.36l-1.9-2.28a1 1 0 0 0-.768-.36H5a1 1 0 0 0-1 1Z"/>
+        </svg>
+        Group: Do
+    </p>
+    <div class="tooltip-arrow" data-popper-arrow></div>
+</div>
+*/
+const tooltipTemplate = function (taskId, task) {
+    return `
+    <button data-tooltip-target="tooltip-bottom-${taskId}" data-tooltip-trigger="click" data-tooltip-placement="bottom" type="button" class="ms-3 mb-2 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">${task.title}</button>
+    <div id="tooltip-bottom-${taskId}" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+        <h3>${task.title}</h3>
+        <p>
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+            </svg>
+            Deadline: ${task.deadline}
+        </p>
+        <p>
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"/>
+            </svg>
+            Description: ${task.description}
+        </p>
+        <p>
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.583 8.445h.01M10.86 19.71l-6.573-6.63a.993.993 0 0 1 0-1.4l7.329-7.394A.98.98 0 0 1 12.31 4l5.734.007A1.968 1.968 0 0 1 20 5.983v5.5a.992.992 0 0 1-.316.727l-7.44 7.5a.974.974 0 0 1-1.384.001Z"/>
+            </svg>
+            Tags: ${task.tag}
+        </p>
+        <p>
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.5 8H4m0-2v13a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1h-5.032a1 1 0 0 1-.768-.36l-1.9-2.28a1 1 0 0 0-.768-.36H5a1 1 0 0 0-1 1Z"/>
+            </svg>
+            Group: ${Dict.groups[Dict.tags[task.tag].groupId].title}
+        </p>
+        <div class="tooltip-arrow" data-popper-arrow></div>
+    </div>
+    `;
+}
+
+const showAllTasksInTable = function (Dict) {
+    // With each day in #CalendarTable, find all tasks that have deadline on that day, then show its title in tooltip truncated
+    // When click on that tooltip, show full description of the task
+    // Tooltip will show deadline, description, tags, group of the task
+
+    // Get all 
 }
 
 
-
 $(document).ready(function () {
-    // Load dict
-
     // If screen width is greater than 768px, show calendar table
     // else hide it
-    if ($(window).width() > 768) {
-        $("#CalendarTable").parent().removeClass("hidden");
-    } else {
-        $("#CalendarTable").parent().addClass("hidden");
-    }
+    // if ($(window).width() > 768) {
+    //     $("#CalendarTable").parent().removeClass("hidden");
+    // } else {
+    //     $("#CalendarTable").parent().addClass("hidden");
+    // }
     mapDatepickerToTable();
+
+    // Load data to calendar
     $.when(loadDict()).done(function (data) {
         // set initial date for #calendar is today
         // format: MM/DD/YYYY'
@@ -385,9 +465,12 @@ $(document).ready(function () {
         console.log('[6] Load data to calendar successfully!');
         console.log(Dict);
 
+        // Get date from focused datepicker-cell
         let date = pickDate();
         console.log(date);
+        // Get all tasks that have deadline on focused day
         changeExpiredColor(getExpiredDays(date[1], date[2]));
+        // Show all tasks that have deadline on focused day
         appendTask(Dict.tasks, date[0], date[1], date[2]);
         console.log(getTask(...date));
 
